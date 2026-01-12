083cb14a8a5b49e983a7ddfb21ed1f242982b287
Ibrahim-Sempy <ibrahim-sempy@users.noreply.github.com>
2026-01-12 14:53:52 +0000

    Organize backend branch: remove frontend-admin and mobile directories

--- frontend-admin/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // NEXT_PUBLIC_ variables are automatically available in the browser
-  // No need to define them in env, they're already accessible via process.env.NEXT_PUBLIC_API_URL
-}
-
-module.exports = nextConfig
-
--- mobile/android/app/src/main/kotlin/com/example/ehk_cotisations/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ehk_cotisations
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
