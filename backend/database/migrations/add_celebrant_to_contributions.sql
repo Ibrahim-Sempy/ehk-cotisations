42609b2f554e084cf95e4dba48a0e4fc1e4ea338
Ibrahim-Sempy <ibrahim-sempy@users.noreply.github.com>
2026-01-12 15:03:44 +0000

    Organize mobile branch: remove backend and frontend-admin directories

--- backend/database/migrations/add_celebrant_to_contributions.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add celebrant field to contributions table
--- This field stores the name of the person who performed the marriage or baptism
-
-ALTER TABLE contributions ADD COLUMN celebrant TEXT;
-
